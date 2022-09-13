fcaa28f8120c765126c1fa3390cc29f10722dc71
hustle2live <87603877+hustle2live@users.noreply.github.com>
2022-09-13 11:50:09 +0300

    Updates

---
+++ precache-manifest.8b922a6e72792091bbd4fbd68986fb16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f46e127026a0f22f404",
+    "url": "/social-network/static/css/main.393d3b93.chunk.css"
+  },
+  {
+    "revision": "3f46e127026a0f22f404",
+    "url": "/social-network/static/js/main.3f46e127.chunk.js"
+  },
+  {
+    "revision": "b73b78321f36c315e081",
+    "url": "/social-network/static/js/1.b73b7832.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "6f6bbb16aec97391aefe120ec5a4e6a2",
+    "url": "/social-network/static/media/user-icon.6f6bbb16.png"
+  },
+  {
+    "revision": "f4c5edcb8b95fd79c2eec102b00fdd3c",
+    "url": "/social-network/index.html"
+  }
+];
